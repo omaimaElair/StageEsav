6157049755cf19a26a45fda6e6cdb4ac99f6ecc5
omaimaelair <omaima.elair@gmail.com>
2019-07-13 15:21:19 +0100

    second

--- application/models/Etudiant_model.php
+++ application/models/Etudiant_model.php
@@ -6,5 +6,16 @@ class Etudiant_model extends CI_Model {
                 $query = $this->db->get('etudiant');
                 return $query->result();
             }
+            public function insert_etudiant($data){
+            	$query=$this->db->insert('etudiant',$data);
+            }
+            public function update_etudiant($id,$data){
+            	$this->db->where('id_etudiant', $id);
+            	$query = $this->db->update('etudiant', $data);
+            }
+            public function delete_etudiant($id){
+            	$this->db->where('id_etudiant',$id);
+            	$this->db->delete('etudiant');
+            }
 }
 ?>
